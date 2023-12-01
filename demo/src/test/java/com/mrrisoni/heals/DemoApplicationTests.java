5bc5b025ba16fd2d02fb41c25e07756daa1a2eba
Vag Katanias <vanguelisk@gmail.com>
2023-12-01 13:37:01 +0200

    java code

---
+++ demo/src/test/java/com/mrrisoni/heals/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrrisoni.heals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
